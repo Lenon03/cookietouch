0133c7a06bdf096d690c6f6d93d1bdf9a823f372
SapientiaFr <airstyle59@gmail.com>
2017-11-18 23:10:29 +0100

    Added enums

---
+++ src/cookie/protocol/enums/PingSmileysEnum.ts
@@ -0,0 +1,18 @@
+/**
+ * Enum for the ping system (PingSmileysEnum)
+ * The number is the smileyId from the database
+ */
+export enum PingSmileysEnum {
+  hello = 1,
+  ready = 4,
+  great = 7,
+  thx = 8,
+  goodJob = 10,
+  outSight = 26,
+  comeCloser = 43,
+  tooFar = 44,
+  retreat = 63,
+  lightBulb = 80,
+  yes = 88,
+  no = 89,
+}

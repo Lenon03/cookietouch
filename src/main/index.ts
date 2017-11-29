d03d378b958148b5615c63ca273d5c16827cf77a
Christopher Yovanovitch <yovano_c@outlook.com>
2017-11-30 00:18:49 +0100

    Reset all

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,23 +2,10 @@ import { app, BrowserWindow, Menu } from "electron";
 import log from "electron-log";
 import { autoUpdater } from "electron-updater";
 
-// -------------------------------------------------------------------
-// Logging
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This logging setup is not required for auto-updates to work,
-// but it sure makes debugging easier :)
-// -------------------------------------------------------------------
 autoUpdater.logger = log;
 (autoUpdater.logger as any).transports.file.level = "info";
 log.info("App starting...");
 
-// -------------------------------------------------------------------
-// Define the menu
-//
-// THIS SECTION IS NOT REQUIRED
-// -------------------------------------------------------------------
 const template: any[] = [];
 if (process.platform === "darwin") {
   // OS X
@@ -38,57 +25,42 @@ if (process.platform === "darwin") {
     ],
   });
 }
-// -------------------------------------------------------------------
-// Open a window that displays the version
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This isn't required for auto-updates to work, but it's easier
-// for the app to show a window than to have to click "About" to see
-// that updates are working.
-// -------------------------------------------------------------------
 
-function sendStatusToWindow(text: string) {
+function logg(text: string) {
   log.info(text);
-  mainWindow.webContents.send("message", text);
 }
 
 autoUpdater.on("checking-for-update", () => {
-  sendStatusToWindow("Checking for update...");
+  logg("Checking for update...");
 });
 autoUpdater.on("update-available", (info) => {
-  sendStatusToWindow("Update available.");
+  logg("Update available.");
 });
 autoUpdater.on("update-not-available", (info) => {
-  sendStatusToWindow("Update not available.");
+  logg("Update not available.");
 });
 autoUpdater.on("error", (err) => {
-  sendStatusToWindow("Error in auto-updater. " + err);
+  logg("Error in auto-updater. " + err);
 });
 autoUpdater.on("download-progress", (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
   log_message = log_message + " - Downloaded " + progressObj.percent + "%";
   log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
-  sendStatusToWindow(log_message);
+  logg(log_message);
 });
 autoUpdater.on("update-downloaded", (info) => {
-  sendStatusToWindow("Update downloaded");
+  logg("Update downloaded");
 });
-/////////////////////
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-// Global reference to mainWindow
-// Necessary to prevent win from being garbage collected
 let mainWindow: BrowserWindow;
 
 function createMainWindow() {
-  // Construct new BrowserWindow
-  const window = new BrowserWindow();
+  const window = new BrowserWindow({show: false});
+  window.maximize();
+  window.show();
 
-  // Set url for `win`
-  // points to `webpack-dev-server` in development
-  // points to `index.html` in production
   const url = isDevelopment
     ? `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`
     : `file://${__dirname}/index.html`;
@@ -113,62 +85,20 @@ function createMainWindow() {
   return window;
 }
 
-// Quit application when all windows are closed
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications to stay open
-  // until the user explicitly quits
   if (process.platform !== "darwin") { app.quit(); }
 });
 
 app.on("activate", () => {
-  // On macOS it is common to re-create a window
-  // even after all windows have been closed
   if (mainWindow === null) { mainWindow = createMainWindow(); }
 });
 
-//
-// CHOOSE one of the following options for Auto updates
-//
-
-// -------------------------------------------------------------------
-// Auto updates - Option 1 - Simplest version
-//
-// This will immediately download an update, then install when the
-// app quits.
-// -------------------------------------------------------------------
-// Create main BrowserWindow when electron is ready
 app.on("ready", () => {
-  autoUpdater.checkForUpdatesAndNotify();
-  // Create the Menu
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
   mainWindow = createMainWindow();
 });
 
-// -------------------------------------------------------------------
-// Auto updates - Option 2 - More control
-//
-// For details about these events, see the Wiki:
-// https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
-//
-// The app doesn't need to listen to any events except `update-downloaded`
-//
-// Uncomment any of the below events to listen for them.  Also,
-// look in the previous section to see them being used.
-// -------------------------------------------------------------------
-// app.on('ready', function()  {
-//   autoUpdater.checkForUpdates();
-// });
-// autoUpdater.on('checking-for-update', () => {
-// })
-// autoUpdater.on('update-available', (info) => {
-// })
-// autoUpdater.on('update-not-available', (info) => {
-// })
-// autoUpdater.on('error', (err) => {
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();
-// })
+app.on("ready", () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});

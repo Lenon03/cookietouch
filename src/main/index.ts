8d751a2c3bd38622a41c7b37d477dea413dfd379
Christopher Yovanovitch <yovano_c@outlook.com>
2017-11-29 18:56:42 +0100

    Updated updater

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,80 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Menu } from "electron";
 import log from "electron-log";
 import { autoUpdater } from "electron-updater";
-log.transports.file.level = "info";
+
+// -------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+// -------------------------------------------------------------------
 autoUpdater.logger = log;
+(autoUpdater.logger as any).transports.file.level = "info";
+log.info("App starting...");
+
+// -------------------------------------------------------------------
+// Define the menu
+//
+// THIS SECTION IS NOT REQUIRED
+// -------------------------------------------------------------------
+const template: any[] = [];
+if (process.platform === "darwin") {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: "About " + name,
+        role: "about",
+      },
+      {
+        accelerator: "Command+Q",
+        click() { app.quit(); },
+        label: "Quit",
+      },
+    ],
+  });
+}
+// -------------------------------------------------------------------
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+// -------------------------------------------------------------------
+
+function sendStatusToWindow(text: string) {
+  log.info(text);
+  mainWindow.webContents.send("message", text);
+}
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", (info) => {
+  sendStatusToWindow("Update downloaded");
+});
+/////////////////////
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -26,12 +98,6 @@ function createMainWindow() {
   }
 
   window.loadURL(url);
-
-  autoUpdater.checkForUpdatesAndNotify();
-
-  // window.webContents.once("did-frame-finish-load", () => {
-  //
-  // });
 
   window.on("closed", () => {
     mainWindow = null;
@@ -60,7 +126,49 @@ app.on("activate", () => {
   if (mainWindow === null) { mainWindow = createMainWindow(); }
 });
 
+//
+// CHOOSE one of the following options for Auto updates
+//
+
+// -------------------------------------------------------------------
+// Auto updates - Option 1 - Simplest version
+//
+// This will immediately download an update, then install when the
+// app quits.
+// -------------------------------------------------------------------
 // Create main BrowserWindow when electron is ready
 app.on("ready", () => {
+  // Create the Menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
   mainWindow = createMainWindow();
+  autoUpdater.checkForUpdatesAndNotify();
 });
+
+// -------------------------------------------------------------------
+// Auto updates - Option 2 - More control
+//
+// For details about these events, see the Wiki:
+// https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
+//
+// The app doesn't need to listen to any events except `update-downloaded`
+//
+// Uncomment any of the below events to listen for them.  Also,
+// look in the previous section to see them being used.
+// -------------------------------------------------------------------
+// app.on('ready', function()  {
+//   autoUpdater.checkForUpdates();
+// });
+// autoUpdater.on('checking-for-update', () => {
+// })
+// autoUpdater.on('update-available', (info) => {
+// })
+// autoUpdater.on('update-not-available', (info) => {
+// })
+// autoUpdater.on('error', (err) => {
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//   autoUpdater.quitAndInstall();
+// })

67c06abd445ffdf5821a19d872ef07d56b928e3b
Eausa <gregoire.pouthier@hotmail.fr>
2018-01-24 22:03:59 +0100

    Equipments tab

--- src/renderer/components/tabs/Character.tsx
+++ src/renderer/components/tabs/Character.tsx
@@ -2,6 +2,7 @@ import Account from "@account";
 import classnames from "classnames";
 import * as React from "react";
 import { Button, Col, Container, Nav, NavItem, NavLink, Row, TabContent, TabPane } from "reactstrap";
+import Equipments from "./Equipments";
 import Jobs from "./Jobs";
 import Spells from "./Spells";
 import Stats from "./Stats";
@@ -52,6 +53,14 @@ export default class Character extends React.Component<ICharacterProps, ICharact
                 Jobs
               </NavLink>
             </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === "3" })}
+                onClick={() => { this.toggle("3"); }}
+              >
+                Equipments
+              </NavLink>
+            </NavItem>
           </Nav>
         </Row>
         <hr />
@@ -66,6 +75,9 @@ export default class Character extends React.Component<ICharacterProps, ICharact
               </TabPane>
               <TabPane tabId="2">
                 <Jobs account={this.props.account} />
+              </TabPane>
+              <TabPane tabId="3">
+                <Equipments account={this.props.account} />
               </TabPane>
             </TabContent>
           </Col>

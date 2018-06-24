9adcb1e251f986b0f994b22488ba86497244648d
Christopher Yovanovitch <yovano_c@outlook.com>
2018-06-24 23:58:48 +0200

    add main styles

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -4,12 +4,14 @@ import BreedsUtility from "@/core/BreedsUtility";
 import MapPoint from "@/core/pathfinder/MapPoint";
 import InventoryHelper from "@/game/character/inventory/InventoryHelper";
 import DTConstants from "@/protocol/DTConstants";
-// import "typeface-roboto";
 import { initialize, presence } from "@renderer/FirebaseHelpers";
 import "@renderer/FontAwesomeIcons";
 import Main from "@renderer/pages/Main";
+import "material-design-icons/iconfont/material-icons.css";
 import * as React from "react";
 import { render } from "react-dom";
+import "typeface-roboto/index.css";
+import "./main.scss";
 
 const app = initialize();
 app

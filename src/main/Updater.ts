78c0a6375a96429e9a19769651078b66d347741a
Christopher YOVANOVITCH <yovano_c@outlook.com>
2018-06-26 09:23:52 +0200

    translate updater

--- src/main/Updater.ts
+++ src/main/Updater.ts
@@ -1,3 +1,4 @@
+import LanguageManager from "@/configurations/language/LanguageManager";
 import { app, dialog } from "electron";
 import log from "electron-log";
 import { autoUpdater } from "electron-updater";
@@ -6,7 +7,7 @@ autoUpdater.logger = log;
 (autoUpdater.logger as any).transports.file.level = "info";
 
 export function appUpdater() {
-  // autoUpdater.channel = "alpha";
+  autoUpdater.channel = "alpha";
   autoUpdater.on("error", err => log.info(err));
   autoUpdater.on("checking-for-update", () => log.info("checking-for-update"));
   autoUpdater.on("update-available", () => log.info("update-available"));
@@ -27,25 +28,27 @@ export function appUpdater() {
   });
 
   autoUpdater.on("update-downloaded", info => {
-    let message =
-      app.getName() +
-      " " +
-      info.releaseName +
-      " is now available. It will be installed the next time you restart the application.";
+    let message = LanguageManager.trans(
+      "releaseAvailable",
+      app.getName(),
+      info.releaseName
+    );
     if (info.releaseNotes) {
-      message += "\n\nRelease notes:\n" + info.releaseNotes;
+      message += LanguageManager.trans("releaseNotes", info.releaseNotes);
     }
     dialog.showMessageBox(
       {
-        buttons: ["Install and Relaunch", "Later"],
+        buttons: [
+          LanguageManager.trans("installRelaunch"),
+          LanguageManager.trans("later")
+        ],
         defaultId: 0,
         detail: message,
-        message:
-          "A new version (" +
-          info.version +
-          ") of " +
-          app.getName() +
-          " has been downloaded",
+        message: LanguageManager.trans(
+          "newVersionDownloaded",
+          info.version,
+          app.getName()
+        ),
         type: "question"
       },
       response => {
